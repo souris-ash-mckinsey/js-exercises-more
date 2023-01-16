d673281b7995f11f1e044813c6378fb5412d3f47
Souris Ash <souris_ash@mckinsey.com>
2023-01-16 15:04:46 +0530

    feat: add largest even number finding function

---
+++ src/largestEvenNumberFinder.js
@@ -0,0 +1,26 @@
+/**
+ * Returns the largest even number in an array of numbers. Returns -1 if there is no
+ * even number in the array.
+ * Expects an array of numbers.
+ * @param {Array<Number>} arr 
+ * @returns the largest even number in given array of numbers; returns -1 if array has no even number.
+ */
+const getLargestEvenNumberInArray = (arr = []) => {
+  arr.sort().filter((elem) => elem % 2 == 0);
+  return arr.length > 0 ? arr[arr.length - 1] : -1;
+};
+
+
+/**
+ * Returns the largest even number in an array of numbers. Returns -1 if there is no
+ * even number in the array.
+ * Expects an array of numbers.
+ * @param {Array<Number>} arr 
+ * @returns the largest even number in given array of numbers; returns -1 if array has no even number.
+ */
+function getLargestEvenNumberInArrayFn(arr = []) {
+  arr.sort().filter((elem) => elem % 2 == 0);
+  return arr.length > 0 ? arr[arr.length - 1] : -1;
+}
+
+module.exports = { getLargestEvenNumberInArray, getLargestEvenNumberInArrayFn };
