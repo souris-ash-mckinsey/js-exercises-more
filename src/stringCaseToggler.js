6c520434547c59167cc60feed1a125a7fa3a7149
Souris Ash <souris_ash@mckinsey.com>
2023-01-16 15:11:04 +0530

    feat: add function to toggle cases of letters in a string

---
+++ src/stringCaseToggler.js
@@ -0,0 +1,40 @@
+/**
+ * Takes a string and changes lowercase characters to uppercase ones and vice-versa.
+ * @param {String} theStr 
+ * @returns A new string in which the cases are inverted. Does not alter passed string.
+ */
+const toggleCaseOfString = (theStr = '') => {
+  let newString = '';
+
+  for (let i = 0; i < theStr.length; i++) {
+    let thisChar = theStr.charAt(i);
+
+    newString 
+      += thisChar == thisChar.toLowerCase() 
+        ? thisChar.toUpperCase() : thisChar.toUpperCase();
+  }
+
+  return newString;
+};
+
+
+/**
+ * Takes a string and changes lowercase characters to uppercase ones and vice-versa.
+ * @param {String} theStr 
+ * @returns A new string in which the cases are inverted. Does not alter passed string.
+ */
+function toggleCaseOfStringFn(theStr = '') {
+  let newString = '';
+
+  for (let i = 0; i < theStr.length; i++) {
+    let thisChar = theStr.charAt(i);
+
+    newString 
+      += thisChar == thisChar.toLowerCase() 
+        ? thisChar.toUpperCase() : thisChar.toUpperCase();
+  }
+
+  return newString;
+}
+
+module.exports = { toggleCaseOfString, toggleCaseOfStringFn };
